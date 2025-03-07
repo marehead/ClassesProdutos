bf4fb963e7af35f1ad75f0a11c18ad651ad751d2
Mare Dos Santos <96207587+marehead@users.noreply.github.com>
2025-03-07 17:09:58 -0300

    Add files via upload

---
+++ Classes/Produto/Produto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Produto
+{
+    internal class Produto
+    {
+        Public string nome;
+        Public double preco;
+        Public int quantidade; 
+        Public Produto(int Quantidade, double Preco, string Nome)
+        { 
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+         }
+    public double = ValorTotalEstoque() { }
+        Return Preco * Quantidade;
+
+}
+    public void ExibirDetalhesProduto()
+        {
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Nome: {Preco}");
+            Console.WriteLine($"Nome: {Quantidade}");
+        }
+
+            }
+}
---
+++ Classes/Produto/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Produto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto chocolate = new Produto("Cacau Show", 30.00, 100);
+            Produto refrigerante = new Produto("Pepsi", 8.00, 500);
+
+            Produto chinelo = new Produto();
+
+            chinelo.Nome = "Havaianas";
+            chinelo.Preco = 40.00;
+            chinelo.Quantidade = 50;
+
+            refrigerante.Preco = 10.00;
+
+            refrigerante.ExibirDetalhesProduto();
+            Console.WriteLine();
+            chocolate.ExibirDetalhesProduto();
+            Console.WriteLine();
+            chinelo.ExibirDetalhesProduto();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Valor total em estoque de {chocolate.Nome}: {chocolate.ValorTotalEstoque()}")
+            chocolate.ValorTotalEstoque();
+        }
+    }
+}
